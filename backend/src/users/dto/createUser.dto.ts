f5d89d2f277c169d68978ad09f37d665cb09b334
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-02-07 23:39:37 +0700

    chore: update dto

--- backend/src/users/dto/createUser.dto.ts
+++ backend/src/users/dto/createUser.dto.ts
@@ -16,7 +16,7 @@ export class CreateUserDto {
   @IsOptional()
   @Length(2, 200, { message: 'Строка должна включать от 2 до 200 символов' })
   @IsString()
-  about: string;
+  about?: string;
 
 	@IsOptional()
 	@IsUrl()

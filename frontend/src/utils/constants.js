a726d6ea4cc928cc7325236e78d18a20cfe63b11
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-02-07 23:26:55 +0700

    chore: update URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = process.env.REACT_APP_BACKEND_URL;
+export const URL = 'https://api.kulembetov.kpd.nomoredomainswork.ru';
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;

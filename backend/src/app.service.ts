988ac2564d256ead25cd6863c855a08e26af0459
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-02-09 15:13:04 +0700

    chore: update app service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,26 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(clientIp: string): string {
-    return `Добро пожаловать! Ваш IP адрес ${clientIp}. Наслаждайтесь нашим сервисом!`;
+    if (!clientIp) {
+      throw new Error('IP адрес не найден');
+    }
+
+    const formattedIp = clientIp.startsWith('::ffff:')
+      ? clientIp.replace('::ffff:', '')
+      : clientIp;
+
+    const messages = [
+      `Ваш IP адрес ${formattedIp}`,
+      `https://kulembetov.kpd.nomoredomainswork.ru`,
+      'Наслаждайтесь нашим сервисом!',
+    ];
+
+    console.log(messages.join('\n'));
+
+    console.log(`Ваш IP адрес ${clientIp}`);
+    console.log(`https://kulembetov.kpd.nomoredomainswork.ru`);
+    console.log('Наслаждайтесь нашим сервисом!');
+
+    return `Добро пожаловать! Ваш IP адрес ${formattedIp}. Наслаждайтесь нашим сервисом!`;
   }
 }

b15b7cea4a9275b4a18194f78ffcfaf6ac59c6a6
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-02-11 19:26:34 +0700

    chore: update auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,26 +6,29 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-	constructor(
-		private jwtService: JwtService,
-		private usersService: UsersService
-	) {}
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
 
-	auth(user: User) {
-		const payload = { sub: Number(user.id) };
-		return { access_token: this.jwtService.sign(payload) };
-	}
+  auth(user: User) {
+    const payload = { sub: Number(user.id) };
+    return { access_token: this.jwtService.sign(payload) };
+  }
 
-	async validatePassword(username: string, password: string) {
-		const user = await this.usersService.findUserForValidatePassword({
-			where: { username }
-		});
-		if (!(await bcrypt.compare(password, user.password)) || !user) {
-			throw new UnauthorizedException({
-				description: 'Неверный логин или пароль'
-			});
-		}
-		delete user.password;
-		return user;
-	}
+  async validatePassword(username: string, password: string) {
+    const user = await this.usersService.findUserForValidatePassword({
+      where: { username },
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException({
+        description: 'Неверный логин или пароль',
+      });
+    }
+
+    delete user.password;
+
+    return user;
+  }
 }
